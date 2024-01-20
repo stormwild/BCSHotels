23cd87c3e6836b1bc2b8c46938068df390564822
Alexander R Torrijos <artorrijos@gmail.com>
2024-01-20 12:09:45 +0800

    Created Initial FineHotels solution and project

---
+++ src/FineHotels/FineHotelsWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace FineHotelsWeb.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
